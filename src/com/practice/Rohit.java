fd3d421f7e6cd20930f537e2e8ba8f145e16f3ee
shabbirkhan <khanshabbir126@gmail.com>
2026-07-15 23:02:12 +0530

    commit

--- src/com/practice/Rohit.java
+++ src/com/practice/Rohit.java
@@ -2,9 +2,13 @@ package com.practice;
 
 public class Rohit {
 
+	static long sumOfSeries(int n) {
+        long s = (long)n * (n + 1) / 2;
+        return s * s;
+    }
 	
 	public static void main(String[] args) {
 
-		
+		System.out.println(sumOfSeries(5));
 	}
 }
